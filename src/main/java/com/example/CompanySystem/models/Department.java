26796ccce53cfff72e98d4bfc707c383e45405fa
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-12-04 10:35:09 +0000

    Added Department model

--- src/main/java/com/example/CompanySystem/models/Department.java
+++ src/main/java/com/example/CompanySystem/models/Department.java
@@ -1,15 +1,56 @@
 package com.example.CompanySystem.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 
+@Entity
+@Table(name = "departments")
 public class Department {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "department")
     private ArrayList<Employee> employees;
 
     public Department(String name) {
         this.name = name;
         this.employees = new ArrayList<>();
+    }
+
+    public Department() {
+
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public ArrayList<Employee> getEmployees() {
+        return this.employees;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmployees(ArrayList<Employee> employees) {
+        this.employees = employees;
     }
 
 }
